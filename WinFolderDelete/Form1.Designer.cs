55998bad74bea5d6875df8e4af5841dfc1de9bc1
Pontarr <pontarr@gmail.com>
2015-07-02 13:07:07 +0300

    First commit

---
+++ WinFolderDelete/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WinFolderDelete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(376, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Run!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(358, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 70);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 162);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.DoubleClick += new System.EventHandler(this.textBox2_DoubleClick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 238);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(358, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 275);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(475, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Double click to add parent directory";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(357, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = " Enter a list of folders to delete in parent folder (no commas etc, ) in column\r\n" +
+    "";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(110, 17);
+            this.toolStripStatusLabel1.Text = "Victor Bidnenko 2015 ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsTab = true;
+            this.textBox3.Location = new System.Drawing.Point(215, 70);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(248, 162);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "Errors here:";
+            this.textBox3.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 297);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bulldoze";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WinFolderDelete/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFolderDelete
+{
+    public partial class Form1 : Form
+    {
+        public string[] text1;
+
+        public void GetData()
+        {
+            text1 = new string[textBox2.Lines.Length];
+            textBox2.ReadOnly = true;
+            string sum = "";
+
+
+            //System.Windows.Forms.MessageBox.Show(Convert.ToString(textBox2.Lines.Length), "Info",
+            //      System.Windows.Forms.MessageBoxButtons.OK,
+            //      System.Windows.Forms.MessageBoxIcon.Information);
+            if (textBox2.Lines.Length < 1000)
+            {
+                for (int i = 0; i < textBox2.Lines.Length; i++)
+                {
+                    text1[i] = textBox2.Lines[i];
+                    sum += " " + text1[i];
+                }
+
+                string textres = Convert.ToString(textBox2.Lines.Length);
+                //System.Windows.Forms.MessageBox.Show("Added " + sum + " strings!", "Info",
+                //System.Windows.Forms.MessageBoxButtons.OK,
+                //System.Windows.Forms.MessageBoxIcon.Information);
+
+            }
+            else System.Windows.Forms.MessageBox.Show("More than we can handle", "Error",
+                       System.Windows.Forms.MessageBoxButtons.OK,
+                       System.Windows.Forms.MessageBoxIcon.Information);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string DelText = "";
+            int count = 0;
+            GetData();
+            try
+            {
+                progressBar1.Maximum = textBox2.Lines.Length;
+
+                for (int i = 0; i < textBox2.Lines.Length; i++)
+                {
+                    if (!String.IsNullOrEmpty(text1[i]))
+                    {
+                        DelText = textBox1.Text + "\\" + text1[i];
+
+                        //System.Windows.Forms.MessageBox.Show(DelText, "Error",
+                        //System.Windows.Forms.MessageBoxButtons.OK,
+                        //System.Windows.Forms.MessageBoxIcon.Information);
+                    }
+
+                    if (System.IO.Directory.Exists(DelText) && (textBox1.Text + "\\" != DelText))
+                    {
+                        try
+                        {
+                            System.IO.Directory.Delete(DelText, true);
+                            count++;
+                            progressBar1.Value = i;
+                            label1.Text = "Deleting " + DelText + " now!";
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Windows.Forms.MessageBox.Show(ex.Message + " Path:" + DelText, "Error",
+                            System.Windows.Forms.MessageBoxButtons.OK,
+                            System.Windows.Forms.MessageBoxIcon.Warning);
+                            textBox3.AppendText(DelText + "\n");
+                        }
+                        finally
+                        {
+
+                        }
+
+                    }
+
+                }
+
+                label1.Text = "Deleted " + Convert.ToString(count) + " folders of " + Convert.ToString(textBox2.Lines.Length) + "!";
+                count = 0;
+
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message + " Path:" + DelText, "Error",
+                System.Windows.Forms.MessageBoxButtons.OK,
+                System.Windows.Forms.MessageBoxIcon.Warning);
+            }
+            progressBar1.Value = 0;
+            textBox2.ReadOnly = false;
+
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox2_DoubleClick(object sender, EventArgs e)
+        {
+            if (textBox2.ReadOnly == false)
+            {
+                textBox2.Clear();
+            }
+
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
